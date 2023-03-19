81905e2d9250eb9a207401c8c547bea247a61113
Pubudu Kodikara <pubudu@leafycode.com>
2023-03-19 12:50:20 +0530

    feat: add eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  plugins: ['jsx-a11y', 'import', 'prettier', '@typescript-eslint'],
+  extends: ["airbnb", "next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
   },
